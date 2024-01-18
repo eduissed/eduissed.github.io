c54679d5d6767323524c76bc456c0dfe843f5d44
josumm <151452315+josumm@users.noreply.github.com>
2024-01-17 22:25:33 -0600

    chat

--- js/script.js
+++ js/script.js
@@ -78,3 +78,6 @@ chatInput.addEventListener("keydown", (e) => {
 sendChatBtn.addEventListener("click", handleChat);
 closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+
+console.log("API_KEY:", API_KEY);
+console.log("Request Options:", requestOptions);
